8e1f07caa3e0c758a14b901a6d21ff6a06d5608c
Rafael Tavares <tavares.rfl@gmail.com>
2025-02-07 19:13:30 -0300

    GET product by id

---
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -0,0 +1,52 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.Product;
+import jakarta.persistence.Column;
+
+public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(Long id, String name, Double price, String description, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.imgUrl = imgUrl;
+    }
+
+    public ProductDTO(Product entity) {
+        id = entity.getId();
+        name = entity.getName();
+        price = entity.getPrice();
+        description = entity.getDescription();
+        imgUrl = entity.getImgUrl();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+}
