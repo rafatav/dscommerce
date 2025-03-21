f391e63922d38bbd7c7e4e3d52aebd4b241ac1db
Rafael Tavares <tavares.rfl@gmail.com>
2025-03-21 14:23:09 -0300

    User Role model

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -13,7 +13,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
