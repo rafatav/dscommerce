8ef8d9cdba63ff16ddd20cba95000acf1c068095
Rafael Tavares <tavares.rfl@gmail.com>
2025-03-21 14:29:56 -0300

    Spring Security

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -60,7 +60,7 @@ INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Alex Gre
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
- 
+
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
